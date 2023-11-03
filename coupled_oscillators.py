44553b6f626cbb37be3bd9592a7ced6d0b3522b2
Emmanuel Yankson <emmanuelyankson16@gmail.com>
2023-11-03 14:06:04 -0400

    Made changes to include complete calculations for coupled oscillators

--- coupled_oscillators.py
+++ coupled_oscillators.py
@@ -1,41 +1,32 @@
 import numpy as np
-import math
 
 
+# Number of oscillators
+n = int(input('Enter the number of oscillators: ')) 
 
-class Matrix_Calulator:
-    def __init__(self, matrix=[]):
-        self.matrix = matrix
-    
-    '''Constructs a matrix using numpy'''
-    def matrix_constructor(self):
-        new_matrix = np.matrix(self.matrix)
-        return new_matrix
-    
-    '''Returns the determinate of the constructed matrix'''
-    def determinate_calculator(self):
-        new_matrix = self.matrix_constructor()
-        det = np.linalg.det(new_matrix)
-        return print(det)
-        
 
-'''Uses the parent class of matrix calculator in order to construct matricies and calculate determinates'''
-class Oscillations(Matrix_Calulator):
-    def __init__(self, mass, spring_constant, coupling_constant): #initializes the mass, spring, and coupling const.
-        super().__init__(matrix=[])
-        self.mass = mass
-        self.spring_constant = spring_constant
-        self._coupling_constant = coupling_constant
-    
+# Define frequencies 
+omega = input("Enter frequencies separated by comma: ")
 
-    '''the excitation of two frequencies simultaneously, the sum of separate oscillating solutions becomes 
-    the oscilation value for the equation of motion'''
-    def angular_frequency(self, other):
-        omega_1s = math.sqrt((self.spring_constant/self.mass))
-        omega_2s = math.sqrt((other.spring_constant/other.mass))
-        omega = ((omega_1s + omega_2s)/2)
-        return omega
-    
-    def matrix_of_oscilation(self):
-        pass
-    
+try:
+    omega_array = np.array(omega.split(','), dtype=float)
+except:
+    ValueError
+
+# Define coupling strengths 
+Coupling_values = input("Enter coupling values separated by commas (defined as k_ij and x_i): ")
+input_list = Coupling_values.split(',')
+Coupling_matrix = np.array(input_list, dtype=float).reshape(n, n)
+
+# Construct the matrix for the eigenvalue problem
+M = np.diag(omega_array**2) - Coupling_matrix
+
+# Find eigenvalues and eigenvectors
+eigenvalues, eigenvectors = np.linalg.eig(M)
+
+num_columns = eigenvectors.shape[1]
+
+# Iterate through the columns and print each column
+for i in range(num_columns):
+    column_i = eigenvectors[:, i]
+    print("Column", i + 1, ":", column_i)
